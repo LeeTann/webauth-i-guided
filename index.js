4b251b4c7f26bb20b83bdbab1afdcee7121ad595
ltann <leeroyleeroy18@gmail.com>
2019-03-24 14:58:29 -0700

    Added bcyptjs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs')
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -17,6 +18,10 @@ server.get('/', (req, res) => {
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
+  
+  const hash = bcrypt.hashSync(user.password, 14)
+
+  user.password = hash
 
   Users.add(user)
     .then(saved => {
@@ -33,7 +38,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });

323665f7004fe8ea4e93e66d63cecab624471fe1
ltann <leeroyleeroy18@gmail.com>
2019-03-27 18:28:30 -0700

    Added restricted function to get request

--- index.js
+++ index.js
@@ -49,7 +49,29 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+function restricted(req, res, next) {
+  const { username, password } = req.headers
+
+  if (username && password) {
+      Users.findBy({ username })
+      .first()
+      .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+              next()
+          } else {
+              res.status(401).json({ message: 'You shall not pass!?!?!'})
+          }
+      })
+      .catch(error => {
+          res.status(500).json({ message: 'Unexpected error'})
+      })
+  } else {
+      res.status(400).json({ message: 'No credentials provided'})
+  }
+}
+
+server.get('/api/users', restricted, (req, res) => {
+
   Users.find()
     .then(users => {
       res.json(users);
@@ -57,5 +79,5 @@ server.get('/api/users', (req, res) => {
     .catch(err => res.send(err));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
